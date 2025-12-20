b767c212ad188c17641fee6e53b4d2b52af35558
AR Wiley <98679479+AR-Wiley@users.noreply.github.com>
2025-12-19 21:42:50 -0600

    Add files via upload

---
+++ Get-RecentFile.ps1
@@ -0,0 +1,46 @@
+﻿function Get-RecentFile {
+
+    param (
+        [string]$directoryPath = "C:\",
+        [string]$fileExtension = "*.txt"
+    )
+
+    if(-not(Test-Path -Path $directoryPath)){
+        Write-Host "Directory Path does not exist"
+        return
+    }
+
+    $fileErrorLog = Join-Path $env:TEMP "file_error_log.txt"
+
+    if(-not(Test-Path -Path $fileErrorLog)){
+        try{
+            New-Item -Path $fileErrorLog -ItemType File
+        } catch {
+            Write-Host "An error occurred: $($_.Exception.Message)"
+        }       
+    }
+
+    $newestFile = Get-ChildItem -Path $directoryPath -Filter $fileExtension -Recurse -ErrorAction SilentlyContinue 2> $fileErrorLog
+    $sortedFiles = $newestFile | Sort-Object LastWriteTime -Descending
+
+    $startDate = (Get-Date).AddDays(-1)
+    $endDate = Get-Date
+
+    try {
+        $lastFile = $newestFile | Select-Object -First 1
+    } catch {
+        Write-Host "Error: $($_.Exception.Message)"
+    }
+
+    try{
+        $LastDay = $newestFile | Where-Object { $_.LastWriteTime -gt $startDate -and $_.LastWriteTime -lt $endDate }
+    } catch {
+        Write-Host "Error: $($_.Exception.Message)"
+    }
+
+    $lastFile | Select-Object LastWriteTime, Length, Name, FullName
+    $LastDay  | Select-Object LastWriteTime, Length, Name, FullName
+
+}
+
+ 
